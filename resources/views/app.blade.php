e3bd3ade6e8fa72cffbcbc8f1352aaebc31800e0
Sunil G. Gawai <sunilgawai7397@gmail.com>
2024-03-12 15:34:05 +0530

    init laravel with tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="w-full h-screen">
+    <div class="container flex flex-col items-center justify-center space-y-8 my-8">
+        <h1 class="font-bold text-4xl">
+            Welcome to Pizza Shop
+        </h1>
+
+        <button class="rounded-md bg-yellow-400 py-2 px-4 font-semibold text-white hover:bg-yellow-500">
+            Order Now
+        </button>
+    </div>
+</body>
+
+</html>
